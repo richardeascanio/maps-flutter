5eaa8f2d315bd4bcb0c78d21a7f434086236fb79
Richard Ascanio C <richardascanio@correo.unimet.edu.ve>
2021-03-22 16:33:17 -0400

    implementing location bloc to listen location change

--- lib/bloc/my_location/my_location_bloc.dart
+++ lib/bloc/my_location/my_location_bloc.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:meta/meta.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'my_location_event.dart';
 part 'my_location_state.dart';
@@ -10,8 +11,34 @@ part 'my_location_state.dart';
 class MyLocationBloc extends Bloc<MyLocationEvent, MyLocationState> {
   MyLocationBloc() : super(MyLocationState());
 
+  final _geolocator = new Geolocator();
+  StreamSubscription<Position> _positionSubscription;
+
+  void initLocationListener() {
+
+    final locationOptions = LocationOptions(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    );
+
+    _positionSubscription = this._geolocator.getPositionStream(locationOptions).listen((Position position) {
+      print(position);
+      final newLocation = new LatLng(position.latitude, position.longitude);
+      add(OnLocationChanged(newLocation));
+    });
+  }
+
+  void cancelLocationListener() {
+    _positionSubscription?.cancel();
+  }
+
   @override
   Stream<MyLocationState> mapEventToState(MyLocationEvent event) async* {
-    // TODO: implement mapEventToState
+    if (event is OnLocationChanged) {
+      yield state.copyWith(
+        locationExists: true,
+        location: event.location
+      );
+    }
   }
 }
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MapPage extends StatelessWidget {
+import 'package:maps_app/bloc/my_location/my_location_bloc.dart';
+
+class MapPage extends StatefulWidget {
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  @override
+  void initState() { 
+    context.read<MyLocationBloc>().initLocationListener();
+    super.initState();
+  }
+
+  @override
+    void dispose() {
+      context.read<MyLocationBloc>().cancelLocationListener();
+      super.dispose();
+    }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('MapPage'),
+      body: BlocBuilder<MyLocationBloc, MyLocationState>(
+        builder: (_, state) => createMap(state),
       ),
     );
+  }
+
+  Widget createMap(MyLocationState state) {
+    if (!state.locationExists) return Center(child: Text('Locating...'));
+
+    return Text('${state.location.latitude}, ${state.location.longitude}');
   }
 }
