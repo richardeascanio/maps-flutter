4b5da1a2f01851afe1d7a6cd6139916294bb4a69
Richard Ascanio C <richardascanio@correo.unimet.edu.ve>
2021-03-22 15:54:53 -0400

    creating location bloc

---
+++ lib/bloc/my_location/my_location_state.dart
@@ -0,0 +1,26 @@
+part of 'my_location_bloc.dart';
+
+@immutable
+class MyLocationState {
+
+  final bool following;
+  final bool locationExists;
+  final LatLng location;
+
+  MyLocationState({
+    this.following = true,
+    this.locationExists = false,
+    this.location
+  });
+
+  MyLocationState copyWith({
+    bool following,
+    bool locationExists,
+    LatLng location
+  }) => new MyLocationState(
+    following: following ?? this.following,
+    locationExists: locationExists ?? this.locationExists,
+    location: location ?? this.location,
+  );
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:maps_app/bloc/my_location/my_location_bloc.dart';
+
 import 'package:maps_app/pages/access_gps_page.dart';
 import 'package:maps_app/pages/loading_page.dart';
 import 'package:maps_app/pages/map_page.dart';
- 
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: LoadingPage(),
-      routes: {
-        'map': (_) => MapPage(),
-        'loading': (_) => LoadingPage(),
-        'access_gps': (_) => AccessGpsPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => MyLocationBloc(),)
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        home: LoadingPage(),
+        routes: {
+          'map': (_) => MapPage(),
+          'loading': (_) => LoadingPage(),
+          'access_gps': (_) => AccessGpsPage(),
+        },
+      ),
     );
   }
 }
